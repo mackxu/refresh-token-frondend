daddaf746f907a6e5f003715a797545547fcd3e9
duoduoxu <duoduoxu@tencent.com>
2024-12-29 22:41:56 +0800

    feat: 无感刷新token

--- README.md
+++ README.md
@@ -1,50 +1,7 @@
-# React + TypeScript + Vite
+# Refresh Token
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+access_token 用于身份认证，refresh_token 用于刷新 token，也就是续签。
 
-Currently, two official plugins are available:
+在登录接口里同时返回 access_token 和 refresh_token，access_token 设置较短的过期时间，比如 30 分钟，refresh_token 设置较长的过期时间，比如 7 天。
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default tseslint.config({
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
-```
-
-- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
-- Optionally add `...tseslint.configs.stylisticTypeChecked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and update the config:
-
-```js
-// eslint.config.js
-import react from 'eslint-plugin-react'
-
-export default tseslint.config({
-  // Set the react version
-  settings: { react: { version: '18.3' } },
-  plugins: {
-    // Add the react plugin
-    react,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended rules
-    ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
-  },
-})
-```
+当 access_token 失效的时候，可以用 refresh_token 去刷新，服务端会根据其中的 userId 查询用户数据，返回新 token。
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react'
+import axios, { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+
+type PendingTask = {
+  config: AxiosRequestConfig;
+  resolve: (value: unknown) => void;
+}
+
+let isRefreshing = false;
+let pendingTasks: PendingTask[] = [];
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
@@ -11,6 +19,7 @@ const refreshToken = async () => {
   const res = await axios.get(`/user/refresh?token=${refreshToken}`);
   localStorage.setItem('accessToken', res.data.accessToken);
   localStorage.setItem('refreshToken', res.data.refreshToken);
+  return res;
 };
 
 axios.interceptors.request.use((config) => {
@@ -26,10 +35,29 @@ axios.interceptors.response.use((response) => {
   return response;
 }, async (error) => {
   const { status, config } = error.response;
-  console.log(config);
+  if (isRefreshing) {
+    return new Promise((resolve) => {
+      pendingTasks.push({
+        config,
+        resolve,
+      });
+    });
+  }
   if (status === 401 && !config.url.includes('/user/refresh?token=')) {
-    await refreshToken();
-    return axios(error.config);
+    isRefreshing = true;
+    const res = await refreshToken();
+    isRefreshing = false;
+    if (res.status === 200) {
+      pendingTasks.forEach((task) => {
+        task.resolve(axios(task.config));
+      });
+      pendingTasks = [];
+      // 最初的请求
+      return axios(config);
+    } else {
+      return Promise.reject(res.data);
+    }
+
   }
   return Promise.reject(error);
 });
@@ -52,12 +80,15 @@ function App() {
   useEffect(() => {
     const fn = async () => {
       const aaaRes = await axios.get('/aaa');
-      const bbbRes = await axios.get('/bbb');
+      axios.get('/bbb');
+      axios.get('/ccc');
+      axios.get('/ddd');
+      axios.get('/eee');
 
       console.log(aaaRes);
 
       setAaa(aaaRes.data);
-      setBbb(bbbRes.data);
+      // setBbb(bbbRes.data);
     }
     fn();
   }, [])
